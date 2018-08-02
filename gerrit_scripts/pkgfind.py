a2a6dcd333c4429407625c2e713338e0d477e32f
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2018-08-02 20:06:26 +0300

    Add initial argparse and logging support

--- gerrit_scripts/pkgfind.py
+++ gerrit_scripts/pkgfind.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import argparse
 import json
+import logging
 import sys
 
 import requests
@@ -11,6 +13,7 @@ CHANGELOG_URL = ('{base_url}/gitweb?p=packaging/specs/{project}.git;'
                  'a=blob_plain;f=xenial/debian/changelog;'
                  'hb=refs/heads/{branch}')
 
+LOG = logging.getLogger('pkgfind')
 
 def get_change(change_id):
     change = requests.get(GERRIT_CHANGE.format(base_url=GERRIT_BASE,
@@ -34,9 +37,26 @@ def parse_changelog(project, branch, short_sha):
     if earliest:
         return earliest.split()[1].strip('()')
 
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description=('Using Geriit Change-Id, find the oldest package version '
+                     'that contains this commit')
+    )
+    parser.add_argument(
+        'change',
+        help=('Change id on gerrit.')
+    )
+    args = parser.parse_args()
+    return args
+
 
 def main():
-    change_id = sys.argv[1]
+    logging.basicConfig(
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
+    LOG.setLevel(logging.INFO)
+    args = parse_args()
+    change_id = args.change
     change = get_change(change_id)
     project = change['project'].split('/')[-1]
     branch = change['branch']
