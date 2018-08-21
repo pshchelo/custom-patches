4ba9dd47760a97c15665d3f2c5daf9c005e8e5f1
Pavlo Shchelokovskyy <shchelokovskyy@gmail.com>
2018-08-21 12:25:12 +0300

    Improve pkgfind script
    - safe-encode url queries
    - fetch single change instead of querying for list
    - add helpstring on what argument might be
    - allow to pass gerrit URL

--- gerrit_scripts/pkgfind.py
+++ gerrit_scripts/pkgfind.py
@@ -4,28 +4,34 @@ import argparse
 import json
 import logging
 import sys
+import urllib.parse
 
 import requests
 
 GERRIT_BASE = 'https://gerrit.mcp.mirantis.net'
-GERRIT_CHANGE = '{base_url}/changes/?o=CURRENT_REVISION&q=change:{change}'
+GERRIT_CHANGE = '{base_url}/changes/{change}/?o=CURRENT_REVISION'
 CHANGELOG_URL = ('{base_url}/gitweb?p=packaging/specs/{project}.git;'
                  'a=blob_plain;f=xenial/debian/changelog;'
                  'hb=refs/heads/{branch}')
 
 LOG = logging.getLogger('pkgfind')
 
-def get_change(change_id):
-    url = GERRIT_CHANGE.format(base_url=GERRIT_BASE, change=change_id)
+# TODO(pas-ha) use more common code from gerrit_api
+
+
+def get_change(gerrit_url, change_id):
+    url = GERRIT_CHANGE.format(base_url=gerrit_url,
+                               change=urllib.parse.quote(change_id, safe=''))
     LOG.debug('querying gerrit as {}'.format(url))
     change = requests.get(url)
     if change.status_code < 400:
-        return json.loads(change.text[4:])[0]
+        return json.loads(change.text[4:])
 
 
 def parse_changelog(project, branch, short_sha):
     url = CHANGELOG_URL.format(base_url=GERRIT_BASE,
-                               branch=branch, project=project)
+                               branch=urllib.parse.quote(branch, safe=''),
+                               project=urllib.parse.quote(project, safe=''))
     LOG.debug('querying git as {}'.format(url))
     changelog = requests.get(url).text.splitlines()
     earliest = None
@@ -39,15 +45,22 @@ def parse_changelog(project, branch, short_sha):
     if earliest:
         return earliest.split()[1].strip('()')
 
+
 def parse_args():
     parser = argparse.ArgumentParser(
-        description=('Using Geriit Change-Id, find the oldest package version '
-                     'that contains this commit')
+        description=('Using Geriit change-id, find the oldest package '
+                     'version that contains this commit')
     )
     parser.add_argument(
         'change',
-        help=('Change-Id on gerrit.')
+        help=('Gerrit change-id on gerrit: '
+              'full id (<project>~<branch>~<Change-Id>), or'
+              '<Change-Id> if it uniquely identifies change, or '
+              'legacy numeric change id (like http(s)://<gerrit-url>/c/NNNN).')
     )
+    parser.add_argument('--gerrit',
+                        default=GERRIT_BASE,
+                        help='Base Gerrit URL')
     args = parser.parse_args()
     return args
 
@@ -58,10 +71,9 @@ def main():
     )
     LOG.setLevel(logging.INFO)
     args = parse_args()
-    change_id = args.change
-    change = get_change(change_id)
+    change = get_change(args.gerrit, args.change)
     if not change:
-        LOG.error('Change {} is not found'.format(change_id))
+        LOG.error('Change {} is not found'.format(args.change))
         sys.exit(1)
     project = change['project'].split('/')[-1]
     branch = change['branch']
